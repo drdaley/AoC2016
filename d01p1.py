b64d6a29237bf0ed03a7c82d9a9f55f627160186
drdaley <mail.david.daley@gmail.com>
2016-12-01 22:01:21 -0500

    Update d01p1.py

--- d01p1.py
+++ d01p1.py
@@ -10,13 +10,14 @@ compass = ['N','E','S','W']
 
 for t in range(len(sys.argv[1:])):
   heading = list(sys.argv[t+1])
+  distance = float((''.join(heading[1:])).strip(','))
   if heading[0] == 'R':
       facing = compass[((compass.index(facing)+4)%3)]
   elif heading[0] == 'L':
       facing = compass[((compass.index(facing)-2)%3)]
-  if facing == 'N': y += float(''.join(heading[1:]))
-  elif facing == 'E': x += float(''.join(heading[1:]))
-  elif facing == 'S': y -= float(''.join(heading[1:]))
-  elif facing == 'W': x -= float(''.join(heading[1:]))
+  if facing == 'N': y += distance
+  elif facing == 'E': x += distance
+  elif facing == 'S': y -= distance
+  elif facing == 'W': x -= distance
 
 print(abs(x)+abs(y))

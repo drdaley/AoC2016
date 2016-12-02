fbcbd062384fc65c481f9ef8cd9b8d95a03e3c03
drdaley <mail.david.daley@gmail.com>
2016-12-01 21:36:59 -0500

    Update d01p1.py

--- d01p1.py
+++ d01p1.py
@@ -6,17 +6,17 @@ x = 0
 y = 0
 # starting direction
 facing = 'N'
-compass = list['NESW']
+compass = ['N','E','S','W']
 
-for t in range(sys.argv[1:]):
-  heading = list(sys.argv[t])
+for t in range(len(sys.argv[1:])):
+  heading = list(sys.argv[t+1])
   if heading[0] == 'R':
-    facing = compass[(compass.index(facing)+1)]
+      facing = compass[((compass.index(facing)+4)%3)]
   elif heading[0] == 'L':
-      facing = compass[(compass.index(facing)-1)]
-  if facing == 'N': y += heading[1]
-  elif facing == 'E': x += heading[1]
-  elif facing == 'S': y -= heading[1]
-  elif facing == 'W': x -= heading[1]
+      facing = compass[((compass.index(facing)-2)%3)]
+  if facing == 'N': y += float(''.join(heading[1:]))
+  elif facing == 'E': x += float(''.join(heading[1:]))
+  elif facing == 'S': y -= float(''.join(heading[1:]))
+  elif facing == 'W': x -= float(''.join(heading[1:]))
 
 print(abs(x)+abs(y))

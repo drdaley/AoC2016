dbc009639ff7aa9a8401321485a2df9b357b011a
drdaley <mail.david.daley@gmail.com>
2016-12-01 20:23:09 -0500

    Update d01p1.py

--- d01p1.py
+++ d01p1.py
@@ -10,13 +10,13 @@ compass = list['NESW']
 
 for t in range(sys.argv[1:]):
   heading = list(sys.argv[t])
-    if heading[0] == 'R':
-      facing = compass[(compass.index(facing)+1)]
-    elif heading[0] == 'L':
+  if heading[0] == 'R':
+    facing = compass[(compass.index(facing)+1)]
+  elif heading[0] == 'L':
       facing = compass[(compass.index(facing)-1)]
-  if facing == 'N' y += heading[1]
-  elif facing == 'E' x += heading[1]
-  elif facing == 'S' y -= heading[1]
-  elif facing == 'W' x -= heading[1]
+  if facing == 'N': y += heading[1]
+  elif facing == 'E': x += heading[1]
+  elif facing == 'S': y -= heading[1]
+  elif facing == 'W': x -= heading[1]
 
 print(abs(x)+abs(y))

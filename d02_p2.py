ff49ac4b19a94298a01be5f558f5371ccc8dfcab
drdaley <mail.david.daley@gmail.com>
2016-12-03 16:46:46 -0500

    Update d02_p2.py
    works now

--- d02_p2.py
+++ d02_p2.py
@@ -1,22 +1,29 @@
 #Bathroom Security : part 2
-keypad= [[5,z,3,z,1],
+#define strings
+z='z'
+A='A'
+B='B'
+C='C'
+D='D'
+#define keypad
+keypad= [[5,z,2,z,1],
          [z,6,z,3,z],
          [A,z,7,z,4],
-         [z,B,z,2,z],
+         [z,B,z,8,z],
          [D,z,C,z,9]]
 #start at keypad[0][0]
 direction = ['U','D','L','R']
 move = [(-1,1),(1,-1),(-1,-1),(1,1)]
 code= list()
+nextmove = [0,0]
+coordinates = [0,0]
 def digit():
-  coordinates = [0,0]
   for i in range(len(instruction)):
-    if (for d in range(len(coordinates)): coordinates[d] += move[direction.index(instruction[i])][d] >= 0) and (for d in range(len(coordinates)): coordinates[d] += move[direction.index(instruction[i])][d] <= 0) :
+    for k in range(len(coordinates)): 
+      nextmove[k] = (coordinates[k] + move[direction.index(instruction[i])][k])
+    if all(j <= 4 for j in nextmove) and all(j >= 0 for j in nextmove):   
       for d in range(len(coordinates)):
-        coordinates[d] += move[direction.index(instruction[i])][d]
-#    for d in range(len(coordinates)):
-#      if coordinates[d] > 4 : coordinates[d] = 4
-#      if coordinates[d] < 0 : coordinates[d] = 0
+          coordinates[d] += move[direction.index(instruction[i])][d]
   code.append(keypad[coordinates[0]][coordinates[1]])
   
 input = open('input.txt') 

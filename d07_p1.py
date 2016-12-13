5f010b40716df2f93ccbe6d1cbed5ad199e834d7
unknown <mail.david.daley@gmail.com>
2016-12-12 23:31:43 -0500

    d07_p1 works now

--- d07_p1.py
+++ d07_p1.py
@@ -3,10 +3,8 @@ def check(s, value):
   for c in range(len(s)-3):
     
     if s[c] != s[c+1] and s[c:c+2] == str(s[c+3]+s[c+2]): 
-      #print(s[c:c+4])
       value.append('T')
-  
-with open('input.txt') as data:
+with open('..\input\input.txt') as data:
   linedata = data.read().splitlines()
   counter = 0
   for line in linedata:
@@ -14,21 +12,19 @@ with open('input.txt') as data:
     outside = list()
     scratch = line
     while '[' in scratch:
-      print(scratch[0:scratch.find('[')])
       outside.append(scratch[0:scratch.find('[')])
-      print(scratch[scratch.find('[')+1:scratch.find(']')])
       inside.append(scratch[scratch.find('[')+1:scratch.find(']')])
-      scratch = scratch.lstrip(scratch[0:scratch.find(']')+1])
-    print(scratch)
-    if len(scratch) > 0 : outside.append(scratch)
+      scratch = scratch[(scratch.find(']')+1):]
+    if scratch == '': print('Problem!')
+    outside.append(scratch)
     outvalue = list()
     invalue = list()
-    #for s in outside:
-    check(''.join(outside), outvalue)
-    #for s in inside:
-    check(''.join(inside), invalue)
-    print(line)
+    for o in outside:
+      check(o, outvalue)
+    for i in inside:
+      check(i, invalue)
+    print(outside)
+    print(inside)
     if 'T' in outvalue and 'T' not in invalue: 
-      
       counter = counter + 1
-  print(counter)  
+print(counter)  

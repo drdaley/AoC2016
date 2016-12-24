27390ac75bb4156727a5e7b579dc5ce098245926
unknown <mail.david.daley@gmail.com>
2016-12-23 23:36:55 -0500

    d13_p2 works

--- d13_p2.py
+++ d13_p2.py
@@ -1,38 +1,29 @@
 # Day 13: A Maze of Twisty Little Cubicles
-# How many coordinates visited in 50 steps?
+# How many coordinates are within 50 steps?
 
-def pathtest(x,y,n,b,pos,f): #uses the rules from the prompt to return True for a floor and False for a wall and test whether the square is within the allowed range and test whether the path has been traveled to yet
-  return (str("{0:b}".format(x*x + 3*x + 2*x*y + y + y*y + numkey)).count('1')%2 == 0) # this line tests for floor or wall
-         and x >= 0
-         and y >= 0
-         and (x,y) not in b
-         and (x,y) not in pos
-         and (x,y) not in f
-def move(x,y,d):
-  if d=u return x,y+1
-  if d=d return x,y-1
-  if d=l return x-1,y
-  if d=r return x+1,y
+def pathtest(i,n,b,pos,f): #uses the rules from the prompt to return True for a floor and False for a wall and test whether the square is within the allowed range and test whether the path has been traveled to yet
+  x = i[0]
+  y = i[1]
+  return (str("{0:b}".format(x*x + 3*x + 2*x*y + y + y*y + numkey)).count('1')%2 == 0) and x >= 0 and y >= 0 and (x,y) not in b and (x,y) not in pos and (x,y) not in f
+  
+def genmoves(x,y): # takes x,y from adjacent and returns a list of tuples of all coordinates adjacent to c
+  return [(x,y+1),(x,y-1),(x-1,y),(x+1,y)]
+  
 def adjacent(pos,c,b,f,n):
-  #print(c[0])
-  if pathtest(move(c[0],c[1],u),n):
-    f.append((c[0]+1,c[1]))
-  if pathtest(c[0]-1,c[1],n)  and  c[0]-1 >= 0 and c[1] >= 0 and (c[0]-1,c[1]) not in b and (c[0]-1,c[1]) not in pos and (c[0]-1,c[1]) not in f:
-    f.append((c[0]-1,c[1]))
-  if pathtest(c[0],c[1]+1,n)  and c[0] >= 0 and c[1]+1 >= 0 and (c[0],c[1]+1) not in b and (c[0],c[1]+1) not in pos and (c[0],c[1]+1) not in f:
-    f.append((c[0],c[1]+1))
-  if pathtest(c[0],c[1]-1,n)  and c[0] >= 0 and c[1]-1 >= 0 and (c[0],c[1]-1) not in b and (c[0],c[1]-1) not in pos and (c[0],c[1]-1) not in f:
-    f.append((c[0],c[1]-1))
+  moves = genmoves(c[0],c[1])
+  for i in moves:
+    if pathtest(i,n,b,pos,f): #pathtest each potential move
+      f.append(i)             #if move passes, append the coordinate to forwards
+
 numkey = 1352 
 start = (1,1)
 pos = [start]
-backwards = list()
+backwards = [(1,1)]
 steps = 0
 for i in range(50):
   forwards = list()
   for c in pos:
     adjacent(pos, c, backwards, forwards, numkey)
-  backwards.extend(pos)
   pos = forwards
-print(backwards)
+  backwards.extend(pos)
 print(len(backwards)) 

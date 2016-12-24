9bd3158e7a30f985c501c9c905b91f1d68f690e6
unknown <mail.david.daley@gmail.com>
2016-12-23 23:02:34 -0500

    d13 has basic parts, but does not work and is a cluster

--- d13_p2.py
+++ d13_p2.py
@@ -1,11 +1,21 @@
 # Day 13: A Maze of Twisty Little Cubicles
 # How many coordinates visited in 50 steps?
 
-def pathtest(x,y,n): #uses the rules from the prompt to return True for a floor and False for a wall
-  return (str("{0:b}".format(x*x + 3*x + 2*x*y + y + y*y + numkey)).count('1')%2 == 0)
+def pathtest(x,y,n,b,pos,f): #uses the rules from the prompt to return True for a floor and False for a wall and test whether the square is within the allowed range and test whether the path has been traveled to yet
+  return (str("{0:b}".format(x*x + 3*x + 2*x*y + y + y*y + numkey)).count('1')%2 == 0) # this line tests for floor or wall
+         and x >= 0
+         and y >= 0
+         and (x,y) not in b
+         and (x,y) not in pos
+         and (x,y) not in f
+def move(x,y,d):
+  if d=u return x,y+1
+  if d=d return x,y-1
+  if d=l return x-1,y
+  if d=r return x+1,y
 def adjacent(pos,c,b,f,n):
   #print(c[0])
-  if pathtest(c[0]+1,c[1],n) and c[0]+1 >= 0 and c[1] >= 0 and (c[0]+1,c[1]) not in b and (c[0]+1,c[1]) not in pos and (c[0]+1,c[1]) not in f:
+  if pathtest(move(c[0],c[1],u),n):
     f.append((c[0]+1,c[1]))
   if pathtest(c[0]-1,c[1],n)  and  c[0]-1 >= 0 and c[1] >= 0 and (c[0]-1,c[1]) not in b and (c[0]-1,c[1]) not in pos and (c[0]-1,c[1]) not in f:
     f.append((c[0]-1,c[1]))

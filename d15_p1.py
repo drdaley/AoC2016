781a8f0e21a5d0d54fb01eb50742e9672728be3e
unknown <mail.david.daley@gmail.com>
2016-12-27 17:46:49 -0500

    d15_p1 works

---
+++ d15_p1.py
@@ -0,0 +1,27 @@
+
+def lineparse(l):
+  n = int(l[l.find('has')+4:l.find('p')-1])
+  p = int(l[l.find('position ')+9:l.find('.')])
+  return (n, p)
+
+def dropcheck(np, t): #checks each ring if the capsule is dropped at t, returns T if all will allow the capsule passage
+  pos = np.copy()
+  ringcheck = list()
+  for (x,(y,z)) in pos:
+    if (x+z+t)%y == 0:
+      ringcheck.append(True)
+    else: ringcheck.append(False)
+  if all(ringcheck):
+    return True
+  else: return False
+
+with open('..\input\input.txt') as data:
+  np = list()
+  for line in data:
+    np.append(lineparse(line))
+  np = list(enumerate(np, start=1)) # gives each ring a value cooresponding to how long the capsule will take to reach it from being dropped
+  t=0
+  while not dropcheck(np, t):
+    t=t+1
+    print('iteration')
+  print(t)

f7e11f78b9c8f2f0c67c1125510fd0df717db0bd
unknown <mail.david.daley@gmail.com>
2016-12-29 05:00:38 -0500

    doorcheck() is not working

---
+++ d17_p1.py
@@ -0,0 +1,68 @@
+import hashlib
+#pc = input('What is the passcode? ')
+pc = 'dmypynyp'
+c = list('UDLR') #+y,-y,-x,+x
+pos = [0,0] #(x,y)
+end = [4,-4]
+goodpaths = list()
+
+def findpos(path):
+  pos = [0,0]
+  for p in path:
+    if p == 'U' : pos[1] = pos[1]+1
+    elif p == 'D' : pos[1] = pos[1]-1
+    elif p == 'L': pos[0] = pos[0]-1
+    elif p =='R' : pos[0] = pos[0]+1
+  return pos
+  
+def opencheck(h): #from [0:4] of the hash, returns a list, to be zipped with c, of T/F for unlocked/locked
+  open = list()
+  for x in h:
+    if x == 'b' or x == 'c' or x == 'd' or x == 'e' or x == 'f':
+      open.extend([True])
+    else: open.extend([False])
+  return open
+
+def doorcheck(pos): #from pos, returns a list, to be zipped with c, of T/F if a door exists/doesn't
+  doors = list()
+  if pos[1] == 0: doors.extend([False,True])
+  elif pos[1] == -4: doors.extend([True,False])
+  elif 0 > pos[1] > -4: doors.extend([True,True])
+  if pos[0] == 0: doors.extend([False,True])
+  elif pos[0] == 4: doors.extend([True,False])
+  elif 0 > pos[0] > 4: doors.extend([True,True])
+  return doors
+  
+def endcheck(gp):
+  for g in gp:
+    if findpos(g) == (4,-4): return True
+  return False
+
+h = hashlib.md5((pc).encode(encoding="utf-8")).hexdigest()
+open = list()
+open = list(zip(c, opencheck(h[0:4]), doorcheck(pos)))
+#print(open)
+for (d,o,t) in open:
+  if all([o,t]): goodpaths.append(d)
+#print(goodpaths)
+s = 1 #steps    
+while not endcheck(goodpaths):
+  newpaths = list()
+  for path in goodpaths:
+    pos = findpos(path)
+    print(doorcheck(pos))
+    h = str()
+    h = hashlib.md5((pc+path).encode(encoding="utf-8")).hexdigest()
+    open = list()
+    open = list(zip(c, opencheck(h[0:4]), doorcheck(pos)))
+    #print(open)
+    for (d,o,t) in open:
+      print((d,o,t))
+      print(all([o,t]))
+      if all([o,t]): 
+        print(str(path+d))
+        newpaths.append(str(path+d))
+  goodpaths = newpaths
+  #print(goodpaths)
+  s = s + 1  
+print(s)

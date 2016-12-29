f56307e780f475c1bcff0cca405d434e777b6466
unknown <mail.david.daley@gmail.com>
2016-12-29 18:34:23 -0500

    d17_p1 works

--- d17_p1.py
+++ d17_p1.py
@@ -1,9 +1,10 @@
 import hashlib
 #pc = input('What is the passcode? ')
 pc = 'dmypynyp'
+#pc = 'ihgpwlah'
 c = list('UDLR') #+y,-y,-x,+x
 pos = [0,0] #(x,y)
-end = [4,-4]
+end = [3,-3]
 goodpaths = list()
 
 def findpos(path):
@@ -26,22 +27,26 @@ def opencheck(h): #from [0:4] of the hash, returns a list, to be zipped with c, 
 def doorcheck(pos): #from pos, returns a list, to be zipped with c, of T/F if a door exists/doesn't
   doors = list()
   if pos[1] == 0: doors.extend([False,True])
-  elif pos[1] == -4: doors.extend([True,False])
-  elif 0 > pos[1] > -4: doors.extend([True,True])
+  elif pos[1] == -3: doors.extend([True,False])
+  elif 0 > pos[1] > -3: doors.extend([True,True])
+  #print(doors)
   if pos[0] == 0: doors.extend([False,True])
-  elif pos[0] == 4: doors.extend([True,False])
-  elif 0 > pos[0] > 4: doors.extend([True,True])
+  elif pos[0] == 3: doors.extend([True,False])
+  elif 0 < pos[0] < 3: doors.extend([True,True])
+  #print(doors)
   return doors
   
 def endcheck(gp):
   for g in gp:
-    if findpos(g) == (4,-4): return True
+    if findpos(g) == [3,-3]: 
+      print('The correct path is: '+g)
+      return True
   return False
 
 h = hashlib.md5((pc).encode(encoding="utf-8")).hexdigest()
 open = list()
 open = list(zip(c, opencheck(h[0:4]), doorcheck(pos)))
-#print(open)
+print(open)
 for (d,o,t) in open:
   if all([o,t]): goodpaths.append(d)
 #print(goodpaths)
@@ -50,19 +55,22 @@ while not endcheck(goodpaths):
   newpaths = list()
   for path in goodpaths:
     pos = findpos(path)
-    print(doorcheck(pos))
+    #print(doorcheck(pos))
     h = str()
     h = hashlib.md5((pc+path).encode(encoding="utf-8")).hexdigest()
     open = list()
     open = list(zip(c, opencheck(h[0:4]), doorcheck(pos)))
+    #print(c)
+    #print(opencheck(h[0:4]))
+    #print(doorcheck(pos))
     #print(open)
     for (d,o,t) in open:
-      print((d,o,t))
-      print(all([o,t]))
+      #print((d,o,t))
+      #print(all([o,t]))
       if all([o,t]): 
-        print(str(path+d))
+        #print(str(path+d))
         newpaths.append(str(path+d))
   goodpaths = newpaths
-  #print(goodpaths)
+  print(goodpaths)
   s = s + 1  
 print(s)

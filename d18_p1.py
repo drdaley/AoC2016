99683d7ee062172df4626059cbb1d36b76b0b029
unknown <mail.david.daley@gmail.com>
2016-12-29 20:09:40 -0500

    cleaned up

--- d18_p1.py
+++ d18_p1.py
@@ -1,5 +1,4 @@
 from pprint import pprint
-finput = '.^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....'
 rows = list()
 rows.append(finput)
 def flooring(r):
@@ -10,24 +9,12 @@ def flooring(r):
     elif p[i-1] == '.' and c == '^' and p[i+1] == '^': next.append('^')
     elif p[i-1] == '^' and c == '.' and p[i+1] == '.': next.append('^')
     elif p[i-1] == '.' and c == '.' and p[i+1] == '^': next.append('^')
-    #if p[i-1] == '' and c == '' and p[i+1] == '':
     else: next.append('.')
-  #print(len(next))
   r.append(''.join(next))
-  #print(len(r))
-  #previous[:0] = '.'
-  #previous.extend('.')
-  #print(previous)
-  #print(len(previous))
+def rowcount(r):
+  return r.count('.') 
 
 while len(rows) < 40:
   flooring(rows)
 
-#print(sum(for r in rows: r.count('.')))
-def rowcount(r):
-  return r.count('.')
-#print(rows.count('.'))
-#print(sum(lambda x: row[x].count('.')))
-#pprint(rows)
-print(sum(map(len, rows)))
 print(sum(map(rowcount, rows)))
